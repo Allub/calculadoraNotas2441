459fed5f63038f8c3bd7ffe091b30f8b133440f6
Franco Madariaga <frankurox@gmail.com>
2020-09-24 12:25:44 -0300

    Finalizar el calculo de porcentaje
    Agregar el calculo de porcentaje
    Agregar el Linear Layout para el resultado
    Almacenar la nota al presionar el boton
    Validar que el porcentaje no exceda a 10
    Mostrar condicionalmente el color del promedio

--- Fuentes/CalculadoraNotas/app/src/main/java/cl/inacap/calculadoranotas/MainActivity.java
+++ Fuentes/CalculadoraNotas/app/src/main/java/cl/inacap/calculadoranotas/MainActivity.java
@@ -2,12 +2,17 @@ package cl.inacap.calculadoranotas;
 
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -15,22 +20,47 @@ import java.util.List;
 import cl.inacap.calculadoranotas.dto.Nota;
 
 public class MainActivity extends AppCompatActivity {
+
+    private int porcentajeActual=0;
     private EditText notaTxt;
     private EditText porcentajeTxt;
     private Button agregarBtn;
     private Button limpiarBtn;
     private ListView notasLv;
     private List<Nota> notas = new ArrayList<>();
+    private ArrayAdapter<Nota> adapter;
+    private TextView promedioTxt;
+    private LinearLayout promedioLl;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        this.promedioTxt = findViewById(R.id.promedioTxt);
+        this.promedioLl = findViewById(R.id.promedioLl);
         this.notaTxt = findViewById(R.id.notaTxt);
         this.porcentajeTxt = findViewById(R.id.porcentajeTxt);
         this.agregarBtn = findViewById(R.id.agregarBtn);
         this.limpiarBtn = findViewById(R.id.limpiarBtn);
         this.notasLv = findViewById(R.id.notasLv);
+        this.adapter = new ArrayAdapter<>(this,android.R.layout. simple_list_item_1,notas);
+        this.notasLv.setAdapter(adapter);
+        this.limpiarBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //limpiar los EditText
+                notaTxt.setText("");
+                porcentajeTxt.setText("");
+                //Ocultar el LinearLayout de resultado
+                promedioLl.setVisibility(View.INVISIBLE);
+                //volver el porcentaje acumulado a 0
+                porcentajeActual = 0;
+                //limpiar la lista
+                notas.clear();
+                adapter.notifyDataSetChanged();
+            }
+        });
+
 
         this.agregarBtn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -38,8 +68,8 @@ public class MainActivity extends AppCompatActivity {
                 List<String> errores = new ArrayList<>();
                 String notaStr = notaTxt.getText().toString().trim();
                 String porcStr = porcentajeTxt.getText().toString().trim();
-                int porcentaje;
-                double nota;
+                int porcentaje = 0;
+                double nota = 0;
                 try {
                     nota = Double.parseDouble(notaStr);
                     if (nota < 1.0 || nota > 7.0) {
@@ -57,20 +87,53 @@ public class MainActivity extends AppCompatActivity {
                 } catch (NumberFormatException ex) {
                     errores.add("-El procentaje debe ser un numero entre 1 y 100");
                 }
-                if (errores.isEmpty()){
+                if (errores.isEmpty()) {
+                    if(porcentaje + porcentajeActual > 100){
+                        //se excede el valor permitido
+                        Toast.makeText(MainActivity.this
+                                , "El porcentaje no puede ser mayor que 100"
+                                , Toast.LENGTH_SHORT).show();
+                    }else{
+                        Nota n = new Nota();
+                        n.setValor(nota);
+                        n.setPorcentaje(porcentaje);
+                        porcentajeActual += porcentaje;
+                        notas.add(n);
+                        //cada vez que modifique el recurso
+                        //asociado al adaptador debo llamar a este metodo
+                        adapter.notifyDataSetChanged();
+                        mostrarPromedio();
+                    }
 
-                }else{
+                } else {
                     mostrarErrores(errores);
                 }
             }
         });
     }
 
+    private void mostrarPromedio(){
+        double promedio=0;
+        for(Nota n:notas){
+            promedio+= (n.getValor()*n.getPorcentaje()/100);
+        }
+        this.promedioTxt.setText(String.format("%.1f",promedio));
+        if(promedio < 4.0){
+            //el texto salga en rojo
+            this.promedioTxt.setTextColor(ContextCompat.getColor(this, R.color.colorError));
+        }else{
+            //el texto salga en azul
+            this.promedioTxt.setTextColor(ContextCompat.getColor(this,R.color.colorExitoso));
+        }
+        this.promedioLl.setVisibility(View.VISIBLE);
+
+    }
+
     private void mostrarErrores(List<String> errores) {
         //generar cadena de texto con los errores
         String mensaje = "";
-        for (String e:errores){
-            mensaje+= "-" + e + "\n";
+        for (String e : errores) {
+            mensaje += "-" + e + "\n";
         }
         AlertDialog.Builder aleBuilder = new AlertDialog.Builder(MainActivity.this);
         //Chaining (encadenamiento)
